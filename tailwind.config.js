bc7d484d4660584a8ef632f8ad2562978407f3ad
jubairskv <jubairskv@gmail.com>
2024-08-08 16:23:26 +0530

    blogcard config done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "color-dark-blue": "#883AE1",
+        "color-light-blue": "#E6D6FC",
+        "color-light-gray": "#E5EAF0",
+        "color-dark-gray": "#6C727F",
+        "color-white": "#FFFFFF",
+        "color-light-white": "#FAFAF9",
+        "color-drak-black": "#20293A",
+      },
+    },
   },
   plugins: [],
-}
+};
