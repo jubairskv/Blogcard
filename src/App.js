b40f70bffe87f3959fb134d25c95e14ad7a18c05
jubairskv <jubairskv@gmail.com>
2024-08-08 17:37:59 +0530

    blogcard config done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import MainContainer from './components/MainContainer';
 import './App.css';
 
 function App() {
   return (
     <div className="bg-color-white">
-      
+      <MainContainer/>
     </div>
   );
 }
---
+++ src/components/MainContainer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const MainContainer = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="w-[30rem] h-[30rem] rounded-lg p-4 shadow-md bg-white">
+        MainContainer
+      </div>
+    </div>
+  );
+};
+
+export default MainContainer;
